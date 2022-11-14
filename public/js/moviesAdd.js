6bdd0dabac3e6143f5fd877e6619a96096933f98
WaelS87 <wfakhrji@gmail.com>
2022-11-14 12:34:07 -0300

    secreto magico

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,12 +1,45 @@
-window.onload = function(){
-    let titulo = document.querySelector('.moviesAddTitulo')
-    let formulario = document.querySelector('#formulario');
-    let article = document.querySelector('article');
-    titulo.innerHTML = 'AGREGAR PELÍCULA';
-    titulo.classList.add('titulo');
-    article.classList.add('fondoTransparente');
-    formulario.classList.add('fondoCRUD');
+window.onload = function () {
+  let titulo = document.querySelector(".moviesAddTitulo");
+  let formulario = document.querySelector("#formulario");
+  let article = document.querySelector("article");
+  titulo.innerHTML = "AGREGAR PELÍCULA";
+  titulo.classList.add("titulo");
+  article.classList.add("fondoTransparente");
+  formulario.classList.add("fondoCRUD");
 
+  let inputTitulo = document.querySelector("#titulo");
+  let estadoSecreto = 0;
+  let checkKey = (key) => {
+    switch (true) {
+      case key === "s" && estadoSecreto === 0:
+        ++estadoSecreto;
+        break;
+      case key === "e" && estadoSecreto === 1:
+        ++estadoSecreto;
+        break;
+      case key === "c" && estadoSecreto === 2:
+        ++estadoSecreto;
+        break;
+      case key === "r" && estadoSecreto === 3:
+        ++estadoSecreto;
+        break;
+      case key === "e" && estadoSecreto === 4:
+        ++estadoSecreto;
+        break;
+      case key === "t" && estadoSecreto === 5:
+        ++estadoSecreto;
+        break;
+      case key === "o" && estadoSecreto === 6:
+        estadoSecreto = 0;
+        alert("SECRETO MAGICO");
+        break;
 
-
-}
+      default:
+        estadoSecreto = 0;
+        break;
+    }
+  };
+  inputTitulo.addEventListener("keydown", (e) => {
+    checkKey(e.key);
+  });
+};
